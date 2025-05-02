1c2a820dd284f8cf2bf129f1830b718930e81156
Oliver Mao <olivermao2005@gmail.com>
2025-05-02 17:14:44 -0400

    Added infinite scroll pagination. Currently a bug where the scrolling retrieves the same data infinitely

--- src/components/JournalEntryScroll.jsx
+++ src/components/JournalEntryScroll.jsx
@@ -1,11 +1,24 @@
-import JournalItem from './JournalItem'
+import { useEffect, useRef } from 'react';
+import JournalItem from './JournalItem';
 
-function JournalEntryScroll({entries}) {
+function JournalEntryScroll({entries, loadMore}) {
+    const scrollRef = useRef(null);
+
+    useEffect(() => {
+        const scroll = scrollRef.current;
+        scroll.addEventListener('scroll',() => {
+            if (scroll.scrollHeight - scroll.scrollTop - scroll.clientHeight < 32) {
+                loadMore();
+            }
+        })
+    }, []);
+
     return (
-        <div className="h-full flex flex-col gap-5 overflow-y-scroll p-4" style={{scrollbarWidth: 'none'}}>
+        <div ref={scrollRef} className="h-full flex flex-col gap-5 overflow-y-scroll p-4" style={{scrollbarWidth: 'none'}}>
             {entries.map((el, idx) => {
                 return <JournalItem item={el} key={idx}/>
             })}
+            <div className="h-32"/>
         </div>
     )
 }
--- src/pages/JournalPage.jsx
+++ src/pages/JournalPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useCallback, useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 import { Input, Typography, Button } from '@mui/material';
@@ -12,15 +12,19 @@ function QueryPage() {
     const [ lastSeen, setLastSeen ] = useState(-1);
 
     useEffect(() => {
-        async function fetchData() {
-            const entriesRaw = await axios.get(`http://localhost:3000/journal/loadMore/${lastSeen}`);
-            console.log(entriesRaw);
-            setEntries((prev) => prev.concat(entriesRaw.data.journalEntries));
+        loadMore();
+    }, []);
+
+    useEffect(() => {
+        if (entries.length > 0) {
             setLastSeen(entries[entries.length-1]._id);
         }
+    }, [entries]);
 
-        fetchData();
-    }, []);
+    const loadMore = async () => {
+        const entriesRaw = await axios.get(`http://localhost:3000/journal/loadMore/${lastSeen}`);
+        setEntries((prev) => prev.concat(entriesRaw.data.journalEntries));
+    }
 
     async function sendJournalEntry(entry) {
         if (entry == "") {
@@ -44,8 +48,13 @@ function QueryPage() {
 
     return (
         <div className="w-full h-full overflow-scroll px-12 py-30 flex-1 flex flex-col">
+            <Button onClick={loadMore}>test</Button>
             <h1 className="mb-12">Write a Journal Entry</h1>
-            <JournalEntryScroll className="flex-shrink" entries={entries}/>
+            <JournalEntryScroll
+                className="flex-shrink"
+                entries={entries}
+                loadMore={loadMore}
+            />
             <div className="mt-6 flex flex-row w-full">
                 <Input
                     className="mr-5 flex flex-grow"
