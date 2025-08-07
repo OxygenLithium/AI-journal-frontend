e80f85c09d12da9ad81ab76a486c4a7295741f60
Oliver Mao <olivermao2005@gmail.com>
2025-08-07 02:56:34 -0400

    corrected typo in relevant journal objects

--- src/components/search-page/tabs/RelevantEntriesTab.jsx
+++ src/components/search-page/tabs/RelevantEntriesTab.jsx
@@ -2,8 +2,8 @@ import JournalItem from "../../JournalItem";
 
 function RelevantEntriesTab({ journalEntries }) {
     return (
-        <div className="flex flex-column items-center justify-center h-full w-full">
-            {journalEntries.map(el => <JournalItem item={el.text}/>)}
+        <div className="flex flex-col gap-5 overflow-y-scroll p-4 h-full w-full">
+            {journalEntries.map(el => <JournalItem item={el}/>)}
         </div>
     )
 }
